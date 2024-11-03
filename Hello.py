3385ee7ca392c0dee66101505241a60226b3f81c
Ginny-Dang <ginnydang0728@gmail.com>
2024-11-03 14:52:16 -0600

    added main page

---
+++ Hello.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("Comprehensive Popular AI Tools")
+
+    
+st.write("# Welcome to my AI-powered App 👋")
+
+st.markdown(
+    """
+        List Backend was listed here:
+        - Chabot Backend: [Link Colab](https://colab.research.google.com/drive/1XIZikuY3KtZzfnDG3wEcBBIHJhhdKgU-?usp=sharing)
+        
+    """
+)
